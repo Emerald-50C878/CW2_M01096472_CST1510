6ef498ba7c76da5648872a91b8d6384bcd4dce41
Stevan Goljevac <sstevan.goljevac@gmail.com>
2025-11-05 14:30:24 +0000

    Changed up the code a little bit

--- log_in.py
+++ log_in.py
@@ -4,17 +4,17 @@ username = input("Enter your username: ")
 password = input("Enter your password: ")
 act_user = ["superstevan", "Vaiiorex", "neicowo", "ttruths"]
 act_password = ["CrystalGems100%"]
-loop_var = 1
-while loop_var == 1:
+loop_var = True
+while loop_var == True:
     if username in act_user:
         print(f"Username found! Welcome {username}")
     else:
         print("Username is not registered... Please create an account using the home page.")
-        break
+        loop_var = False
     if password in password:
         print(f"Password correct!")
     else:
         print("Password incorrect, try again.")
-        break
+        loop_var = False
 
     print(f"Login Successful!")
